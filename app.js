cdee28d5b136c4ace145a34b0091395fc5b9371b
josegzza <a01038061@itesm.mx>
2021-01-27 14:15:09 -0600

    chess board texture / using http-server

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var vertexShaderText =
 '',
 'attribute vec3 vertPosition;',
 'attribute vec3 vertColor;',
+'attribute vec2 vertTexCoord;',
+'varying vec2 fragTexCoord;',
 'varying vec3 fragColor;',
 'uniform mat4 mWorld;',
 'uniform mat4 mView;',
@@ -11,6 +13,7 @@ var vertexShaderText =
 '',
 'void main()',
 '{',
+'  fragTexCoord = vertTexCoord;',
 '  fragColor = vertColor;',
 '  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
 '}'
@@ -20,10 +23,14 @@ var fragmentShaderText =
 [
 'precision mediump float;',
 '',
+'varying vec2 fragTexCoord;',
+'uniform sampler2D sampler;',
+'',
 'varying vec3 fragColor;',
 'void main()',
 '{',
-'  gl_FragColor = vec4(fragColor, 1.0);',
+//'  gl_FragColor = vec4(fragColor, 1.0);',
+'  gl_FragColor = texture2D(sampler, fragTexCoord);',
 '}'
 ].join('\n');
 
@@ -90,42 +97,72 @@ var InitDemo = function () {
 	// Create buffer
 	//
 	var boxVertices = 
-	[ // X, Y, Z           R, G, B
-		// Top
-		-0.125, 2.0, -2.0,   0.5, 0.5, 0.5,
-		-0.125, 2.0, 2.0,    0.5, 0.5, 0.5,
-		0.125, 2.0, 2.0,     0.5, 0.5, 0.5,
-		0.125, 2.0, -2.0,    0.5, 0.5, 0.5,
+	[ // X, Y, Z           
+		// Top					U V
+		-0.125, 2.0, -2.0,   0, 0,
+		-0.125, 2.0, 2.0,    0, 1,
+		0.125, 2.0, 2.0,     1, 1,
+		0.125, 2.0, -2.0,    1, 0,
 
-		// Left
-		-0.125, 2.0, 2.0,    0.75, 0.25, 0.5,
-		-0.125, -2.0, 2.0,   0.75, 0.25, 0.5,
-		-0.125, -2.0, -2.0,  0.75, 0.25, 0.5,
-		-0.125, 2.0, -2.0,   0.75, 0.25, 0.5,
+		// Left				
+		-0.125, 2.0, 2.0,    0, 0,
+		-0.125, -2.0, 2.0,   1, 0,
+		-0.125, -2.0, -2.0,  1, 1,
+		-0.125, 2.0, -2.0,   0, 1,
 
 		// Right
-		0.125, 2.0, 2.0,    0.25, 0.25, 0.75,
-		0.125, -2.0, 2.0,   0.25, 0.25, 0.75,
-		0.125, -2.0, -2.0,  0.25, 0.25, 0.75,
-		0.125, 2.0, -2.0,   0.25, 0.25, 0.75,
+		0.125, 2.0, 2.0,    1, 1,
+		0.125, -2.0, 2.0,   0, 1,
+		0.125, -2.0, -2.0,  0, 0,
+		0.125, 2.0, -2.0,   1, 0,
 
 		// Front
-		0.125, 2.0, 2.0,    1.0, 0.0, 0.15,
-		0.125, -2.0, 2.0,    1.0, 0.0, 0.15,
-		-0.125, -2.0, 2.0,    1.0, 0.0, 0.15,
-		-0.125, 2.0, 2.0,    1.0, 0.0, 0.15,
+		0.125, 2.0, 2.0,    1, 1,
+		0.125, -2.0, 2.0,    1, 0,
+		-0.125, -2.0, 2.0,    0, 0,
+		-0.125, 2.0, 2.0,    0, 1,
 
 		// Back
-		0.125, 2.0, -2.0,    0.0, 1.0, 0.15,
-		0.125, -2.0, -2.0,    0.0, 1.0, 0.15,
-		-0.125, -2.0, -2.0,    0.0, 1.0, 0.15,
-		-0.125, 2.0, -2.0,    0.0, 1.0, 0.15,
+		0.125, 2.0, -2.0,    0, 0,
+		0.125, -2.0, -2.0,    0, 1,
+		-0.125, -2.0, -2.0,    1, 1,
+		-0.125, 2.0, -2.0,    1, 0,
 
 		// Bottom
-		-0.125, -2.0, -2.0,   0.5, 0.5, 1.0,
-		-0.125, -2.0, 2.0,    0.5, 0.5, 1.0,
-		0.125, -2.0, 2.0,     0.5, 0.5, 1.0,
-		0.125, -2.0, -2.0,    0.5, 0.5, 1.0,
+		-0.125, -2.0, -2.0,   1, 1, 
+		-0.125, -2.0, 2.0,    1, 0,
+		0.125, -2.0, 2.0,     0, 0,
+		0.125, -2.0, -2.0,    0, 1,
+		
+		// // Left					R, G, B
+		// -0.125, 2.0, 2.0,    0.75, 0.25, 0.5,
+		// -0.125, -2.0, 2.0,   0.75, 0.25, 0.5,
+		// -0.125, -2.0, -2.0,  0.75, 0.25, 0.5,
+		// -0.125, 2.0, -2.0,   0.75, 0.25, 0.5,
+
+		// // Right
+		// 0.125, 2.0, 2.0,    0.25, 0.25, 0.75,
+		// 0.125, -2.0, 2.0,   0.25, 0.25, 0.75,
+		// 0.125, -2.0, -2.0,  0.25, 0.25, 0.75,
+		// 0.125, 2.0, -2.0,   0.25, 0.25, 0.75,
+
+		// // Front
+		// 0.125, 2.0, 2.0,    1.0, 0.0, 0.15,
+		// 0.125, -2.0, 2.0,    1.0, 0.0, 0.15,
+		// -0.125, -2.0, 2.0,    1.0, 0.0, 0.15,
+		// -0.125, 2.0, 2.0,    1.0, 0.0, 0.15,
+
+		// // Back
+		// 0.125, 2.0, -2.0,    0.0, 1.0, 0.15,
+		// 0.125, -2.0, -2.0,    0.0, 1.0, 0.15,
+		// -0.125, -2.0, -2.0,    0.0, 1.0, 0.15,
+		// -0.125, 2.0, -2.0,    0.0, 1.0, 0.15,
+
+		// // Bottom
+		// -0.125, -2.0, -2.0,   0.5, 0.5, 1.0,
+		// -0.125, -2.0, 2.0,    0.5, 0.5, 1.0,
+		// 0.125, -2.0, 2.0,     0.5, 0.5, 1.0,
+		// 0.125, -2.0, -2.0,    0.5, 0.5, 1.0,
 	];
 
 	var boxIndices =
@@ -164,26 +201,59 @@ var InitDemo = function () {
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
 	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+	//var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+	// gl.vertexAttribPointer(
+	// 	positionAttribLocation, // Attribute location
+	// 	3, // Number of elements per attribute
+	// 	gl.FLOAT, // Type of elements
+	// 	gl.FALSE,
+	// 	6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+	// 	0 // Offset from the beginning of a single vertex to this attribute
+	// );
+	// gl.vertexAttribPointer(
+	// 	colorAttribLocation, // Attribute location
+	// 	3, // Number of elements per attribute
+	// 	gl.FLOAT, // Type of elements
+	// 	gl.FALSE,
+	// 	6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+	// 	3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+	// );
+
 	gl.vertexAttribPointer(
 		positionAttribLocation, // Attribute location
 		3, // Number of elements per attribute
 		gl.FLOAT, // Type of elements
 		gl.FALSE,
-		6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
 		0 // Offset from the beginning of a single vertex to this attribute
 	);
 	gl.vertexAttribPointer(
-		colorAttribLocation, // Attribute location
-		3, // Number of elements per attribute
+		texCoordAttribLocation, // Attribute location
+		2, // Number of elements per attribute
 		gl.FLOAT, // Type of elements
 		gl.FALSE,
-		6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
 		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
 	);
 
 	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(colorAttribLocation);
+	gl.enableVertexAttribArray(texCoordAttribLocation);
+	
+	//Create Texture
+	var boxTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(
+		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		document.getElementById('crate-image')
+	);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+
 
 	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
@@ -220,8 +290,12 @@ var InitDemo = function () {
 		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 		
+		gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+		gl.activeTexture(gl.TEXTURE0);
+		
+		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+
 		requestAnimationFrame(loop);
 	};
 	requestAnimationFrame(loop);
