e05426e0cf0f8843444561503a077f8b35bc6527
josegzza <a01038061@itesm.mx>
2021-02-05 16:04:55 -0600

    Move buttons/animation/load game

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var scene, renderer, camera, controls;
-
-var Pieces = {
-};
-
+var Pieces = {};
 var boardPositions = { 
 	"y":{"pawn": 3,"queen":30, "knight":10, "bishop":3, "king":32, "rook":20},
 	"a1":{ "x": -130, "y": 30, "z": 135, "status":false },
@@ -70,21 +67,93 @@ var boardPositions = {
 	"g8":{ "x": 95, "y": 30, "z": -130, "status":false },
 	"h8":{ "x": 130, "y": 30, "z": -130, "status":false }
 };
-
 var games = {
 	"g1":{
 		0 : ["whitePawn5","e4"],
 		1 : ["blackPawn5", "e5"],
 		2 : ["whiteKnight2", "f3"],
-		3 : ["blackKnight1", "c6"]
+		3 : ["blackKnight1", "c6"],
+		4 : ["whiteBishop2", "c4"],
+		5 : ["blackBishop2", "c5"],
+		6 : ["whitePawn3", "c3"],
+		7 : ["blackKnight2", "f6"],
+		8 : ["whitePawn4", "d4"],
+		9 : ["blackPawn5", "d4"],
+		10 : ["whitePawn2", "b4"],
+		11 : ["blackBishop2", "b6"],
+		12 : ["whitePawn5", "e5"],
+		13 : ["blackKnight2", "e4"],
+		14 : ["whiteBishop2", "d5"],
+		15 : ["blackKnight2", "c3"],
+		16 : ["whiteKnight1", "c3"],
+		17 : ["blackPawn5", "c3"],
+		18 : ["whiteBishop1", "g5"],
+		19 : ["blackKnight1", "e7"],
+		20 : ["whiteKing", "g1", "castle", "whiteRook2", "f1"],
+		21 : ["blackPawn8", "h6"],
+		22 : ["whiteBishop1", "h4"],
+		23 : ["blackKing", "g8", "castle", "blackRook2", "f8"],
+		24 : ["whiteRook2", "e1"],
+		25 : ["blackQueen", "e8"],
+		26 : ["whiteBishop2", "b3"],
+		27 : ["blackPawn1", "a5"],
+		28 : ["whiteBishop1", "f6"],
+		29 : ["blackPawn1", "a4"],
+		30 : ["whiteBishop2", "c4"],
+		31 : ["blackKnight1", "g6"],
+		32 : ["whiteQueen", "d3"],
+		33 : ["blackPawn4", "d5"],
+		34 : ["whitePawn5", "d6", "erase", "blackPawn4", "d5"],
+		35 : ["blackBishop1", "e6"],
+		36 : ["whiteQueen", "g6"],
+		37 : ["blackPawn6", "g6"],
+		38 : ["whiteRook2", "e6"],
+		39 : ["blackQueen", "f7"],
+		40 : ["whiteBishop1", "c3"],
+		41 : ["blackKing", "h8"],
+		42 : ["whiteRook2", "e4"],
+		43 : ["blackQueen", "f5"],
+		44 : ["whiteRook2", "e7"],
+		45 : ["blackRook2", "g8"],
+		46 : ["whiteBishop2", "g8"],
+		47 : ["blackRook1", "g8"],
+		48 : ["whitePawn5", "c7"],
+		49 : ["blackQueen", "c2"],
+		50 : ["whiteBishop1", "e5"],
+		51 : ["blackBishop2", "f2"],
+		52 : ["whiteKing", "h1"],
+		53 : ["blackBishop2", "b6"],
+		54 : ["whitePawn8", "h3"],
+		55 : ["blackKing", "h7"],
+		56 : ["whiteRook1", "e1"],
+		57 : ["blackPawn1", "a3"],
+		58 : ["whiteKing", "h2"],
+		59 : ["blackPawn6", "g5"],
+		60 : ["whiteKnight2", "d4"],
+		61 : ["blackQueen", "c4"],
+		62 : ["whiteKnight2", "f5"],
+		63 : ["blackQueen", "b4"],
+		64 : ["whiteRook1", "c1"],
+		65 : ["blackKing", "g6"],
+		66 : ["whiteRook2", "g7"],
+		67 : ["blackKing", "f5"],
+		68 : ["whiteRook2", "g8"],
+		69 : ["blackBishop2", "c7"],
+		70 : ["whiteBishop1", "c7"],
+		71 : ["blackQueen", "b2"],
+		72 : ["whiteRook1", "c5"],
+		73 : ["blackKing", "e4"],
+		74 : ["whiteRook2", "d8"]
 	}
 };
 
+var start = document.getElementById('start')
+var stop = document.getElementById('stop')
+var restart = document.getElementById('restart')
 
-initialBoard();
 init();
 animate();
-
+initialBoard();
 
 //Load piece
 function createPiece(color, name, pos, dicName){
@@ -102,7 +171,7 @@ function createPiece(color, name, pos, dicName){
 		   Pieces[dicName].whereami = pos;
 		   boardPositions[pos].status = true;
 		   boardPositions[pos].piece = dicName;
-		   scene.add(Pieces[dicName]);
+			scene.add(Pieces[dicName])
 	  });
 	});
 }
@@ -111,25 +180,27 @@ function createPiece(color, name, pos, dicName){
 async function moveAnimation(namePiece, newSpot){
 	const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 	
-	var speedX = 0.5;
-	var speedZ = 0.5;
+	var speedX = 1;
+	var speedZ = 1;
 	var newX = parseInt(newSpot.x)
 	var newZ = parseInt(newSpot.z)
 
-	if(Pieces[namePiece].position.x>newSpot.x){
-		speedX = - 0.5;
-	}
+	while(Pieces[namePiece].position.x!=newX || Pieces[namePiece].position.z!= newZ){
+		if(Pieces[namePiece].position.x>newSpot.x){
+			speedX = - 1;
+		}
 
-	if(Pieces[namePiece].position.z>newSpot.z){
-		speedZ = -0.5;
-	}
+		if(Pieces[namePiece].position.z>newSpot.z){
+			speedZ = -1;
+		}
 		if(Pieces[namePiece].position.x!=newX){
 			Pieces[namePiece].position.x += speedX;
 		}
 		if(Pieces[namePiece].position.z!= newZ){
 			Pieces[namePiece].position.z += speedZ;
 		}
-	await sleep(100);
+		await sleep(0.1);
+	}
 }
 
 //Erase piece
@@ -144,14 +215,13 @@ function erasePiece(spot){
 function nextMove(namePiece, newSpot){
 	var lastSpot = Pieces[namePiece].whereami;
 	
-	//console.log(boardPositions[newSpot].status)
-	//If there is a piece in the new spot, erase piece.
-	if(boardPositions[newSpot].status){
-		//erasePiece(newSpot)
-	}
-
 	//move piece to the new spot
 	moveAnimation(namePiece, boardPositions[newSpot])	
+
+	//If there is a piece in the new spot, erase piece.
+	if(boardPositions[newSpot].status){
+		erasePiece(newSpot)
+	}
 
 	//update values
 	boardPositions[lastSpot].status = false //clear last spot
@@ -160,20 +230,26 @@ function nextMove(namePiece, newSpot){
 	boardPositions[newSpot].status = true //ocupate new spot
 }
 
-async function loadGame(game){
-	const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+function loadGame(game, i){
 	if(games[game]){
-		for(i = 0; i < 4; i++){
-			nextMove(games[game][i][0], games[game][i][1])
-			await sleep(8000);
+		if(games[game][i][2]=="castle"){
+			nextMove(games[game][i][0], games[game][i][1]);
+			nextMove(games[game][i][3], games[game][i][4]);
+		}
+		else if(games[game][i][2]=="erase"){
+			nextMove(games[game][i][0], games[game][i][1]);
+			erasePiece(games[game][i][4])
+		}
+		else{
+			nextMove(games[game][i][0], games[game][i][1]);
 		}
 	}
 }
 
+
 //Empty board with initial piecies in order.
 function initialBoard()
 {
-	
 	//White pieces
 	createPiece("white", "rook","a1", "whiteRook1");
 	createPiece("white","rook","h1", "whiteRook2");
@@ -232,15 +308,6 @@ function init()
 	directionalLight.position.set( 0, 0, 1 ).normalize();
 	scene.add(directionalLight);
 
-	
-	
-	//Controls
-	// var rightButton = new THREE.BoxGeometry(10,10,10);
-	// var rightButtonMaterial = new THREE.MeshBasicMaterial();
-	// rightButtonMesh = new THREE.Mesh(rightButton, rightButtonMaterial);
-	// rightButtonMesh.position.set(100,150,10);
-	// scene.add(rightButtonMesh);
-
 	//Camara
     camera = new THREE.PerspectiveCamera (45, width/height, 1, 10000);
     camera.position.y = 250;
@@ -250,21 +317,28 @@ function init()
     controls = new THREE.OrbitControls (camera, renderer.domElement);
 	
 	//View helper
-    var gridXZ = new THREE.GridHelper(400, 15);
-    gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
-    scene.add(gridXZ);
+    // var gridXZ = new THREE.GridHelper(400, 15);
+    // gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
+    // scene.add(gridXZ);
+}
 
+var x = 0;
+//Next move
+start.onclick = function () {
+	loadGame('g1', x);
+	x++;
+}
+
+restart.onclick = function (){
+	window.location.reload();
 }
 
 
-function animate()
+function animate(time)
 {
-	var timer = Date.now() * 0.00025;
     controls.update();
 	requestAnimationFrame(animate); 
-	//console.log(timer)
-	loadGame('g1')
+	TWEEN.update(time)
 	renderer.render (scene, camera);
-	
 }
 
